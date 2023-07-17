498968713dc65a989d117a366c856782d0f1fd23
Tom Horn <hortw1991@gmail.com>
2023-07-16 23:10:20 -0400

    small updates

--- neophiler/src/test/java/com/neophiler/domain/core/user/UserTest.java
+++ neophiler/src/test/java/com/neophiler/domain/core/user/UserTest.java
@@ -3,7 +3,7 @@ package com.neophiler.domain.core.user;
 import com.neophiler.domain.core.exception.ValidationException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserTest {
 
