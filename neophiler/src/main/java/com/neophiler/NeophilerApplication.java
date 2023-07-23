97da68e3ae5b710c7eca259a99c5b0474dde34e0
Tom Horn <hortw1991@gmail.com>
2023-07-23 12:43:44 -0400

    removed not needed stuff from main

--- neophiler/src/main/java/com/neophiler/NeophilerApplication.java
+++ neophiler/src/main/java/com/neophiler/NeophilerApplication.java
@@ -1,19 +1,12 @@
 package com.neophiler;
 
-import com.neophiler.domain.core.user.InternalUserRepository;
-import com.neophiler.domain.core.user.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class NeophilerApplication {
-    @Autowired private InternalUserRepository internalUserRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(NeophilerApplication.class, args);
     }
